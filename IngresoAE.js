8071c8d8ff6d74a4adacd78f15d83b061daffed7
Eduardo <joselincango73@gmail.com>
2024-08-12 17:31:24 -0500

    Finalizacion de la pagina estatica

--- IngresoAE.js
+++ IngresoAE.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
         let selectVALUE = this.value;
 
         if (selectVALUE === "option1") {
-            window.location.href = "IniciarS2.html";
-        } else if (selectVALUE === "option2") {
             window.location.href = "IniciarS.html";
+        } else if (selectVALUE === "option2") {
+            window.location.href = "IniciarS2.html";
         }
     });
 
